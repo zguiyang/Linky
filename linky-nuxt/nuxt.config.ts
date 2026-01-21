60a30e917e8bf4a1627149a88263b7f76eff9fba
zguiyang <zhaoguiyang18@outlook.com>
2026-01-21 12:58:42 +0800

    temp refactor

---
+++ linky-nuxt/nuxt.config.ts
@@ -0,0 +1,27 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxt/eslint', '@nuxt/ui'],
+
+  devtools: {
+    enabled: true,
+  },
+
+  css: ['~/assets/css/main.css'],
+  ui: {
+    fonts: false,
+  },
+  routeRules: {
+    '/': { prerender: true },
+  },
+
+  compatibilityDate: '2025-01-15',
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs',
+      },
+    },
+  },
+})
