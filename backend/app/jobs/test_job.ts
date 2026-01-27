94fade3986eb1c2a22d19a52e5bf69b269a5ffca
zguiyang <zhaoguiyang18@outlook.com>
2026-01-27 12:14:59 +0800

    refactor(jobs): remove unused test job and update package aliases

--- backend/app/jobs/test_job.ts
+++
@@ -1,9 +0,0 @@
-import { Job } from 'adonisjs-jobs'
-
-type TestJobPayload = {}
-
-export default class TestJob extends Job {
-  async handle(_payload: TestJobPayload) {
-    this.logger.info('TestJob job handled')
-  }
-}
