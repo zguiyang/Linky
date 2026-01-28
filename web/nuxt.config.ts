606a435704c468aca0333b8ee6d45c82e62cce77
zguiyang <zhaoguiyang18@outlook.com>
2026-01-28 10:28:20 +0800

    feat(store): add Pinia store for tags and update Nuxt configuration to include Pinia and VueUse modules

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/icon'],
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/icon', '@pinia/nuxt', '@vueuse/nuxt'],
 
   devtools: {
     enabled: true
@@ -34,5 +34,8 @@ export default defineNuxtConfig({
     serverBundle: {
       collections: ['lucide', 'heroicons']
     }
+  },
+  pinia: {
+    storesDirs: ['app/stores/**']
   }
 })
